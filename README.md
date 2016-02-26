45e2dc2005046ab9152522a1de62028ccf7b52cb
Cole Willsea <coleww@gmail.com>
2016-02-25 18:12:14 -0800

    nice

---
+++ README.md
@@ -0,0 +1,22 @@
+DATABAE
+----------------------
+
+a simple localStorage abstraction. There are many like it but this one is mine.
+
+-----------------------------
+
+auto JSON.(stringify/parse)'s stuff so you don't have to care about squat
+
+### general
+- get(key)
+- set(key, val)
+
+### numeric
+- inc(key, amt || 1)  
+- dec(key, amt || 1)
+
+### arrays
+- push(key, el)
+- remove(key, el)
+
+TODO: add whatever other methods i end up needing for this other project lol
---
+++ index.js
@@ -0,0 +1,48 @@
+function get (key) {
+  return JSON.parse(localStorage.getItem(key))
+}
+
+function set (key, object) {
+  var stringCheese = JSON.stringify(object)
+  // if (stringCheese.length > 5242878) throw('something truly awful has happened')
+  localStorage.setItem(key, stringCheese)
+}
+
+function inc (key, amt) {
+  return mathIt(key, (amt || 1))
+}
+
+function dec (key, amt) {
+  return mathIt(key, -(amt || 1))
+}
+
+function mathIt (key, amt) {
+  var oldVal = get(key)
+  // if (typeof oldVal !== 'number') throw('u tried to __crement ' + oldVal + ' but it wasnt having it')
+  var newVal = oldVal + amt
+  set(key, newVal)
+  return newVal
+}
+
+function push (key, el) {
+  var arr = get(key)
+  arr.push(el)
+  set(key, arr)
+}
+
+function remove (key, el) {
+  var arr = get(key)
+  var i = arr.indexOf(el)
+  set(key, arr.splice(i, 1))
+}
+
+module.exports = {
+  get: get,
+  set: set,
+  inc: inc,
+  dec: dec,
+  push: push,
+  remove: remove
+}
+
+
